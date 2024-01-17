39468b4ec4f45f4fa34adfe8b7078440046e44d3
weeelb <liwo8owil@gmail.com>
2024-01-18 00:05:58 +0900

    test gallery

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <h1>Weeelb Gallery Test Account one</h1>
+        <h1 className="text-black">Weeelb Gallery Test Account one</h1>
           <Gallery/>
       </div>
     </main>
